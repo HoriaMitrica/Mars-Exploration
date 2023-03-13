2fce309c1c4850d257ec7323bc677350fe4ad347
Ioana <dumitrescuioana20@gmail.com>
2023-03-13 11:53:37 +0200

    Added Rover.cs Record;

---
+++ Codecool.MarsExploration.MapExplorer/MarsRover/Model/Rover.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.MarsRover.Model;
+
+public record Rover(int ID, Coordinate currentPosition, int sightReach, IEnumerable<Coordinate> resourcesCoordinates);
