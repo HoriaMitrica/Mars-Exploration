f8813c018e2714f43c293bb1b822409446d11489
Ioana <dumitrescuioana20@gmail.com>
2023-03-13 11:50:03 +0200

    Added ConfiguratorValidator.cs, timeOut validator still to be checked;

--- Codecool.MarsExploration.MapExplorer/Configuration/Model/Simulation.cs
+++ Codecool.MarsExploration.MapExplorer/Configuration/Model/Simulation.cs
@@ -3,4 +3,4 @@ using Codecool.MarsExploration.MapGenerator.Calculators.Model;
 
 namespace Codecool.MarsExploration.MapExplorer.Configuration;
 
-public record Simulation(string MapFilePath,Coordinate landingCoordinate,IEnumerable<string> elementsToScan, int numberOfSteps );
+public record Simulation(string MapFilePath,Coordinate landingCoordinate,IEnumerable<string> elementsToScan, int numberOfSteps, int currentStep );
