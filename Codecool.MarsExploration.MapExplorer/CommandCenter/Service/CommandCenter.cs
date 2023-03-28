96001475c410b5949b2e05c70edd93ee8cb239fc
Ioana <dumitrescuioana20@gmail.com>
2023-03-28 12:42:25 +0300

    Created a CommandCenter class - methods to be added remain;

---
+++ Codecool.MarsExploration.MapExplorer/CommandCenter/Service/CommandCenter.cs
@@ -0,0 +1,19 @@
+﻿using Codecool.MarsExploration.MapExplorer.MarsRover.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.CommandCenter.Service;
+
+public class CommandCenter 
+{
+    public int SightReach { get; }
+
+    public List<Rover> Rovers = new List<Rover>();
+    
+    public int ResourcesStored { get; set; }
+
+    public CommandCenter(int sightReach, List<Rover> rovers, int resourcesStored)
+    {
+        SightReach = sightReach;
+        Rovers = rovers;
+        ResourcesStored = resourcesStored;
+    }
+}
