b840fb8cb364d940589b8d95a95106c6994ad423
Ioana <dumitrescuioana20@gmail.com>
2023-03-29 11:11:54 +0300

    Halfway done with Moving the Rover to mine;

--- Codecool.MarsExploration.MapExplorer/Exploration/Service/MiningDirectory/IMiningSteps.cs
+++ Codecool.MarsExploration.MapExplorer/Exploration/Service/MiningDirectory/IMiningSteps.cs
@@ -1,6 +1,13 @@
-﻿namespace Codecool.MarsExploration.MapExplorer.Exploration.Service.MiningDirectory;
+﻿using Codecool.MarsExploration.MapExplorer.MarsRover.Model;
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Exploration.Service.MiningDirectory;
 
 public interface IMiningSteps
 {
-    // public MoveRover()
+    public void MoveRoverToResource(Coordinate resourceToMine, Rover currentRover);
+
+    public void MineResource();
+
+    public void ReturnRoverToCc(CommandCenter.Service.CommandCenter commandCenter, Rover currentRover);
 }
--- Codecool.MarsExploration.MapExplorer/Exploration/Service/MiningDirectory/MiningSimulationSteps.cs
+++ Codecool.MarsExploration.MapExplorer/Exploration/Service/MiningDirectory/MiningSimulationSteps.cs
@@ -1,6 +1,40 @@
-﻿namespace Codecool.MarsExploration.MapExplorer.Exploration.Service.MiningDirectory;
+﻿using System.Diagnostics;
+using Codecool.MarsExploration.MapExplorer.Logger;
+using Codecool.MarsExploration.MapExplorer.MarsRover.Model;
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
 
-public class MiningSimulationSteps
+namespace Codecool.MarsExploration.MapExplorer.Exploration.Service.MiningDirectory;
+
+public class MiningSimulationSteps: IMiningSteps, ILogger
 {
-    
+    public void MoveRoverToResource(Coordinate resourceToMine, Rover currentRover)
+    {
+        int step = 0;
+        if (resourceToMine.X > currentRover.CurrentPosition.X)
+        {
+            while (currentRover.CurrentPosition.X != resourceToMine.X)
+            {
+                step++;
+                currentRover.CurrentPosition =
+                    currentRover.CurrentPosition with { X = currentRover.CurrentPosition.X + 1 };
+                var message = $"STEP: {step}; EVENT MOVING TO MINE; UNIT: {currentRover.ID}, POSITION: [{currentRover.CurrentPosition.X},{currentRover.CurrentPosition.Y}]";
+                Log(message);
+            }
+        }
+    }
+
+    public void MineResource()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ReturnRoverToCc(CommandCenter.Service.CommandCenter commandCenter, Rover currentRover)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Log(string message)
+    {
+        throw new NotImplementedException();
+    }
 }
